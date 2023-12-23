d67dd79015daae0e7529fd9dede4428507e1fa08
abdullah-alwajih <pro.abdullah.alwajih@gmail.com>
2023-12-23 16:45:14 +0300

    Initial commit

---
+++ src/pages/About/About.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {render, screen} from "@testing-library/react";
+import About from "./About";
+
+describe("About component", () => {
+  test("renders the UnderConstruction component", () => {
+    render(<About/>);
+    const underConstructionElement = screen.getByTestId("under-construction");
+    expect(underConstructionElement).toBeInTheDocument();
+  });
+});
